e10d08c75bdaae269d0ceafe82a6dc5906e71fdd
Shekoni Ayomide Shakiru <shekonishakiru@Shekonis-MacBook-Pro.local>
2023-09-07 10:11:44 +0100

    updates

--- utils/mailSender.ts
+++ utils/mailSender.ts
@@ -1,12 +1,19 @@
 import Mailgun from "mailgun.js";
 import formData from 'form-data';
 const mailgun = new Mailgun(formData);
+import qr from 'qrcode'
+import request from "request";
+import fs from 'fs'
 
 export const sendMail = async (to: string, redeemId: string, tokenId: number) => {
     const mg = mailgun.client({
         username: 'api',
         key: process.env.MAILGUN_APIKEY as string,
     });
+
+    const data = await qr.toDataURL('https://arturverse-frontend.vercel.app/nft/${tokenId}')
+   
+
     const { status, message } =  await mg.messages
                 .create(process.env.MAILGUN_DOMAIN as string, {
                     from: process.env.FROM_MAIL as string,
@@ -25,6 +32,7 @@ export const sendMail = async (to: string, redeemId: string, tokenId: number) =>
                                     <p>View your certificate on this platforms: </p>
                                     <div><a href="https://arturverse-frontend.vercel.app/nft/${tokenId}">arturverse-frontend.vercel.app/nft/${tokenId}</a></div>
                                     <div><a href="https://testnets.opensea.io/assets/sepolia/${process.env.CONTRACT_ADDRESS}/${tokenId}">testnets.opensea.io/assets/sepolia/${process.env.CONTRACT_ADDRESS}/${tokenId}</a></div>
+                                    <img src="${data}" width="500px" height="500px"  alt="qr-code" />
                                     
                                     <p>Thank you.</p>
                                 </div>
@@ -36,7 +44,13 @@ export const sendMail = async (to: string, redeemId: string, tokenId: number) =>
                                 </body>
                             </html>
                         
-                            `
+                            `,
+                            attachment: [
+                                {
+                                    filename: "qr-code.png",
+                                     data
+                                }
+                            ]
                 })
         return { status, message }
 }
