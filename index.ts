bc689ed1ef1b4aa77cddd5d9332f6897cb40e716
Shekoni Ayomide Shakiru <shekonishakiru@Shekonis-MacBook-Pro.local>
2023-10-03 23:28:59 +0100

    pdf-bug-fix-v7

--- index.ts
+++ index.ts
@@ -22,7 +22,7 @@ const corsOptions: CorsOptions = {
 const PORT = process.env.PORT || 4000
 
 const app = express()
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 // @ts-ignore
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
